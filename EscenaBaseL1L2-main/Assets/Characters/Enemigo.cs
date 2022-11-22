fd8af73557da553e430cd01e28f36ecbe7633369
frantute <102159238+frantute@users.noreply.github.com>
2022-11-22 11:59:46 -0300

    parte 2 y 3 terminadas

---
+++ EscenaBaseL1L2-main/Assets/Characters/Enemigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Animator animator;
+
+
+    public bool spawneando = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+ 
+    }
+}
