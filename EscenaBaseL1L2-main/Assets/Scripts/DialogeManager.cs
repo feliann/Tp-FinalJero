3ddef83e3e6a0035004e2dc6bda7c22a922e61fb
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-11-23 11:51:17 -0300

    .

---
+++ EscenaBaseL1L2-main/Assets/Scripts/DialogeManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogeManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueUI;
+    [SerializeField]TextMeshProUGUI textoDelDialogo;
+    [SerializeField] TextMeshProUGUI textoBoton;
+    [SerializeField] bool hasTalked;
+
+    [SerializeField] string[] frasesDialogo;
+    [SerializeField] int posicionFrase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NextFrase();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //al entrar activa la UI de diálogo
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            frasesDialogo = other.gameObject.GetComponent<NPCBehavior>().data.dialogueFrases;
+            if (!hasTalked)
+            {
+                textoDelDialogo.text = "Me voy a comer tu cerebro!";
+                dialogueUI.SetActive(true);
+            }
+            else
+            {
+                textoDelDialogo.text = "jajaj alto bot roman";
+                dialogueUI.SetActive(true);
+                textoBoton.text = "Cerrar";
+            }
+        }
+        
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //al salir desactiva la UI de diálogo
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            dialogueUI.SetActive(false);
+        }
+        
+    }
+
+    public void NextFrase()
+    {
+        if (posicionFrase < frasesDialogo.Length)
+        {
+            textoDelDialogo.text = frasesDialogo[posicionFrase];
+            posicionFrase++;
+            if (posicionFrase == frasesDialogo.Length)
+            {
+                textoBoton.text = "Cerrar";
+            }
+        }
+        else
+        {
+            dialogueUI.SetActive(false);
+            hasTalked = true;
+        }
+        
+    }
+}
