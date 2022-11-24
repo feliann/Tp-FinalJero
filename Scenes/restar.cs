a904be545e3011de2fb53f04ec803424996c0776
feliann <101577329+feliann@users.noreply.github.com>
2022-11-24 16:48:09 -0300

    Add files via upload

---
+++ Scenes/restar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class restar : MonoBehaviour
+{
+    public void Restar()
+    {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+            SceneManager.LoadScene("Sample");
+            Time.timeScale = 1f;
+
+    }
+}
